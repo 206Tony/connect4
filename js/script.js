dc3ecfa0ec229fc21482b7f30ec8d6ddf9cfa87a
Anthony Zardis <anthonyzardis@gmail.com>
2019-06-13 12:42:10 -0700

    Reset button Win logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var board = []; 
 var player = 'red';
+var message = 'Player: ' + player + ' you win!';
 var columnTracker = {
   column0: 0,
   column1: 0,
@@ -13,31 +14,19 @@ var columnTracker = {
 
 document.addEventListener('DOMContentLoaded', function(e) {
   board = document.getElementById('board');
-  // red = document.getElementsByClassName('red');
-  // black = document.getElementsByClassName('black');
   restartBtn = document.getElementById('restartbtn'); 
   game = document.getElementsByClassName('game');
-  playGround = document.getElementsByClassName('playground');
+  row = document.getElementsByClassName('row');
+  column = document.getElementsByClassName('column');
+  message = document.getElementById('message');
 
-  board.addEventListener('click', function(e) {
-    console.log("classes of the element we clicked on:", e.target.classList);
-    fillSpot(e);
-    if (endGame(true)) {
-      e.removeEventListener();
-    }
-  })
-  restartBtn.addEventListener('click', function(e) {
-  console.log(e.target);
-  let els = document.getElementsByClassName('player');
-    for (el of els) {
-      el.classList.remove('black', 'red');
-    }
-    init();
-  })
+
+  board.addEventListener('click',fillSpot);
+  restartBtn.addEventListener('click', init);
 });
 
 function init() {
-  board = [];
+  console.log('hello reset')
   columnTracker = {
     column0: 0,
     column1: 0,
@@ -47,56 +36,39 @@ function init() {
     column5: 0,
     column6: 0
   }
+  let squareds = document.querySelectorAll('.playground')
+  squareds.forEach( function(square) {
+    square.classList.remove('red','black')
+  })
+  board.addEventListener('click',fillSpot);
 }
 
-// loop through classlist of element that you just filled in fillSpot
-// filter non winconditions from classlist
 function checkForWin(el, color) {
   console.log("In checkForWin, this is the el:", el);
-  let winConditions = [];
+  let winConditions = Array.from(el.classList).filter(function(condition) {
+    return condition !== 'playground' && condition !== 'red' && condition !== 'black'
+  });
   let squares;
-  winConditions.push(el.classList.item(1));
-  winConditions.push(el.classList.item(2));
-  console.log(winConditions);
-
   for (let condition of winConditions) {
-    if (condition.slice(0,3) === "row") {
-      squares = el.children; //parentElement.children;
-    } else {
-      squares = document.getElementsByClassName(condition);
-    }
-    let count = 1
-    // loop over squares
+    squares = document.getElementsByClassName(condition);
+    let count = 1;
     for (let i = 1; i < squares.length; i++) {
       // if current square classList contains current player
       if (squares[i].classList.contains(player) && squares[i-1].classList.contains(player)) {
-        // increment count
-        console.log("we found a match, incrementing count")
         count++;
       } else {
-        // if they don't match, reset counter to 1
-        console.log("no match")
         count = 1;
       }
-      // at end of loop, if we've found 4 or more, they win
-      if (count >= 3) {
-        // game is over
-        console.log("we won!");
-        endGame();
-        
+      if (count > 3) {
+        endGame(); 
       }
     }
   }
 }
 
-function endGame() {
-  document.getElementsByClassName('board');
-}
-
 function fillSpot(e) {
   let column = e.target.classList[2];
   let row = 6 - columnTracker[column];
-  console.log("this is row:", row);
   if (row > 0) {
     columnTracker[column]++;
     let spot = document.getElementsByClassName(column)[row];
@@ -105,5 +77,12 @@ function fillSpot(e) {
     checkForWin(spot, player);
   } 
 }
+
+function endGame() {
+  board.removeEventListener('click', fillSpot);
+  document.querySelectorAll('message').value = 'Player ' + player + ' you win!';
+}
+
+
 
 
